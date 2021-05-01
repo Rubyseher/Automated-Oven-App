cf5ea7bc014cca082f40f475f48e713f5dab79da
rubyseher <rubyseher2002@gmail.com>
2021-05-01 19:53:51 +0530

    second slider

--- App.js
+++ App.js
@@ -60,16 +60,37 @@ function utensilScreen() {
       title="Solid Button"
       />
       <Icon name="circle" size={30} color="#900" /> */}
-
-      <View style={{ paddingRight: '5%', paddingLeft: '5%',flexDirection:'row',width:'100%'}}>
-        <Image
-          style={{ height: 30 ,width:30 }}
-          source={require('./images/topOven.png')}
-          resizeMode='contain'
-        />
+      <View style={{ paddingRight: '5%', paddingLeft: '5%' }}>
+        <View style={{ flexDirection: 'row', width: '100%' }}>
+          <Image
+            style={{ height: 30, width: 30 }}
+            source={require('./images/OvenTop.png')}
+            resizeMode='contain'
+          />
+          <Text style={{ textAlign: 'right', width: '90%' }}> 180°C </Text>
+        </View>
         <Slider
           value={value}
-          style={{width:'90%'}}
+          style={{ width: '100%' }}
+          maximumTrackTintColor='#dfddff'
+          minimumTrackTintColor='#ff6a00'
+          maximumValue={200}
+          minimumValue={0}
+          trackStyle={{ backgroundColor: '#ff6a00', height: 20, borderRadius: 20 }}
+          onValueChange={(value) => setValue(value)}
+          thumbStyle={{ backgroundColor: 'transparent' }}
+        />
+                <View style={{ flexDirection: 'row', width: '100%' }}>
+          <Image
+            style={{ height: 30, width: 30 }}
+            source={require('./images/OvenBottom.png')}
+            resizeMode='contain'
+          />
+          <Text style={{ textAlign: 'right', width: '90%' }}> 18°C </Text>
+        </View>
+        <Slider
+          value={value}
+          style={{ width: '100%' }}
           maximumTrackTintColor='#dfddff'
           minimumTrackTintColor='#ff6a00'
           maximumValue={200}
@@ -79,6 +100,7 @@ function utensilScreen() {
           thumbStyle={{ backgroundColor: 'transparent' }}
         />
       </View>
+      
     </View>
   );
 }
