68aa7eba3db0c274e031e3de416e3871c878b402
rubyseher <rubyseher2002@gmail.com>
2021-05-02 11:53:09 +0530

    buttons

--- mainScreen.js
+++ mainScreen.js
@@ -1,15 +1,21 @@
 import React, { useState, Fragment } from 'react';
 import { Slider } from 'react-native-elements';
 import { Image, View, Text } from 'react-native';
-import {styles, colors} from './styles'
+import { Button } from 'react-native-elements';
+import { styles, colors } from './styles'
+import Wand from './assets/wand.svg'
 import OvenTop from './assets/Oven Direction Top.svg'
 import OvenBottom from './assets/Oven Direction Bottom.svg'
 import LinearGradient from 'react-native-linear-gradient';
+// import {FontAwesome5 ,Fontisto } from 'react-native-vector-icons'
+import Icon  from 'react-native-vector-icons/FontAwesome5';
+import Ficon from 'react-native-vector-icons/Fontisto';
+
 
 const GradientProgress = (props) => {
     return (
-        <View style={[{ width: '100%', height: 12, backgroundColor: props.trackColor ? props.trackColor : '#e1dddd' },props.trackStyle]}>
-            <LinearGradient colors={[colors.yellow, colors.orange]} start={{ x: 0, y: 0 }} locations={[0.5,1]} style={{ width: `${props.value}%`, height: '100%' }}></LinearGradient>
+        <View style={[{ width: '100%', height: 12, backgroundColor: props.trackColor ? props.trackColor : '#e1dddd' }, props.trackStyle]}>
+            <LinearGradient colors={[colors.yellow, colors.orange]} start={{ x: 0, y: 0 }} locations={[0.5, 1]} style={{ width: `${props.value}%`, height: '100%' }}></LinearGradient>
         </View>
     )
 }
@@ -17,7 +23,7 @@ const GradientProgress = (props) => {
 const TemperatureSlider = (props) => {
     return (
         <Fragment>
-            <View style={{ flexDirection: 'row', width: '100%', marginTop: 15 }}>
+            <View style={{ flexDirection: 'row', width: '100%', marginTop: 7 }}>
                 {props.icon}
                 <Text style={{ textAlign: 'right', width: '90%', color: 'grey' }}> {Math.round(props.handler.value)}°C </Text>
             </View>
@@ -47,12 +53,29 @@ function mainScreen() {
                 source={require('./assets/Plate.jpg')}
                 resizeMode='cover'
             />
-            <GradientProgress value={70} trackColor={colors.white}/>
+            <GradientProgress value={70} trackColor={colors.white} />
             <Text style={styles.title}>Burger</Text>
             <Text style={styles.subtitle}>14 min 34 sec left</Text>
             <View style={{ width: '80%', alignSelf: 'center' }}>
-                <TemperatureSlider icon={<OvenTop height={25} width={25} fill={colors.black} />} handler={{ value: topTemp, setValue: setTopTemp }} />
-                <TemperatureSlider icon={<OvenBottom height={25} width={25} fill={colors.black} />} handler={{ value: bottomTemp, setValue: setBottomTemp }} />
+                <TemperatureSlider icon={<OvenTop height={28} width={28} fill={colors.black} />} handler={{ value: topTemp, setValue: setTopTemp }} />
+                <TemperatureSlider icon={<OvenBottom height={28} width={28} fill={colors.black} />} handler={{ value: bottomTemp, setValue: setBottomTemp }} />
+            </View>
+            <View style={{ flexDirection: 'row', width: '100%',alignContent:'center',justifyContent:'center'}}>
+                <Button
+                    icon={<Wand height={25} width={25} fill={colors.black}/>}
+                    buttonStyle={styles.roundButtonS}
+                    containerStyle={styles.roundButtonPaddingS}
+                />
+                <Button
+                    icon={<Ficon name="pause" size={26} color={colors.darkGrey} />}
+                    buttonStyle={styles.roundButtonM}
+                    containerStyle={[styles.roundButtonPaddingM, {marginLeft:40,marginRight:40}]}
+                />
+                <Button
+                    icon={<Ficon name="close-a" size={16} color={colors.red}/>}
+                    buttonStyle={styles.roundButtonS}
+                    containerStyle={styles.roundButtonPaddingS}
+                />
             </View>
         </View>
     );
--- styles.js
+++ styles.js
@@ -2,8 +2,10 @@ import { StyleSheet } from 'react-native';
 
 export const colors = {
     grey: '#e1dddd',
+    darkGrey: '#464749',
     white: '#fff',
     black: '#000',
+    red: '#c30000',
     orange: '#ff7312',
     yellow: '#ffd800',
     blue: '#3b91fc',
@@ -28,5 +30,51 @@ export const styles = StyleSheet.create({
         backgroundColor: colors.grey,
         height: 12,
         borderRadius: 20,
+    },
+    roundButtonM: {
+        height: 60,
+        width: 60,
+        borderRadius: 30,
+        padding: 0,
+        alignSelf: 'center',
+        backgroundColor: colors.white,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4.84,
+        elevation: 11,
+    },
+    roundButtonPaddingM: {
+        height:80,
+        width:80,
+        padding: 7,
+        // marginTop:10,
+        alignSelf: 'center',
+    },
+    roundButtonS: {
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+        padding: 0,
+        alignSelf: 'center',
+        backgroundColor: colors.white,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    roundButtonPaddingS: {
+        height:50,
+        width:50,
+        padding: 2,
+        // marginTop:10,
+        alignSelf: 'center',
     }
 });
