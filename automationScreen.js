64d8cbe917f5dddd6b125acd752fbb7653bb96b6
rubyseher <rubyseher2002@gmail.com>
2021-05-14 18:20:00 +0530

    automation

--- automationScreen.js
+++ automationScreen.js
@@ -1,11 +1,9 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { styles, colors } from './styles'
-import OvenTop from './assets/Oven Direction Top.svg'
-import OvenBottom from './assets/Oven Direction Bottom.svg'
+
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Slider } from 'react-native-elements';
-import CircularSlider from 'rn-circular-slider'
 import Timeline from './timeline';
 const data= require('./timeline.json')
 
@@ -55,49 +53,14 @@ const BottomTempSlider = (props) => {
 }
 
 export default function automationScreen() {
-    const [topTemp, setTopTemp] = useState(180);
-    const [bottomTemp, setBottomTemp] = useState(80);
-    const [timeSlider, setTimeSlider] = useState(30);
+
     const [items, setitems] = useState(data);
 
     return (
         <View >
             <Text style={styles.heading}>Automator</Text>
 
-            <View style={[styles.autoContainer, {}]}>
-                <View style={{ flexDirection: 'row' }}>
-                    <View style={[styles.detailsCircle, { backgroundColor: colors.yellow }]}>
-                        <Icon name="utensils" size={14} color={colors.white} style={{ padding: 4, alignSelf: 'center' }} />
-                    </View>
-                    <Text style={styles.detailText}> Cook</Text>
-                </View>
-                <View style={{ flexDirection: 'row' }}>
-                    <View style={{ width: '60%', marginLeft: 20 }}>
-                        <TopTempSlider icon={<OvenTop height={28} width={28} fill={colors.black} />} handler={{ value: topTemp, setValue: setTopTemp }} />
-                        <BottomTempSlider icon={<OvenBottom height={28} width={28} fill={colors.black} />} handler={{ value: bottomTemp, setValue: setBottomTemp }} />
-                    </View>
-                    <View style={{paddingLeft:5,justifyContent:'center'}}>
-                        <CircularSlider
-                            step={2}
-                            min={0}
-                            max={90}
-                            value={timeSlider}
-                            onChange={setTimeSlider}
-                            contentContainerStyle={styles.contentContainerStyle}
-                            strokeWidth={4}
-                            buttonBorderColor={colors.blue}
-                            openingRadian={Math.PI / 4}
-                            buttonRadius={8}
-                            radius={40}
-                            linearGradient={[{ stop: '0%', color: colors.blue }, { stop: '100%', color: colors.blue }]}
-                        >
-                            <Text style={styles.value}>{timeSlider}</Text>
-                            <Text style={styles.min}>min</Text>
-                        </CircularSlider>
-                    </View>
-                </View>
-            </View>
-
+        <Timeline items={items}/>
         </View>
     );
 }
--- timeline.js
+++ timeline.js
@@ -1,15 +1,97 @@
-// import React, { useState } from 'react';
-// import { View, Text } from 'react-native';
+import React, { Fragment, useState } from 'react';
+import { View, Text } from 'react-native';
+import { styles, colors } from './styles'
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import OvenTop from './assets/Oven Direction Top.svg'
+import OvenBottom from './assets/Oven Direction Bottom.svg'
+import CircularSlider from 'rn-circular-slider'
+import Slider from '@react-native-community/slider'
 
-// export default function Timeline(props) {
-//     return (
-//         <View >
-//             {
-//                 props.items.map((item, i) => (
-//                     <Text key={i}>{item.name}</Text>
-//                 ))
-//             }
-//         </View>
-//     );
-// }
+const TemperatureSlider = (props) => {
+    return (
+        <Fragment>
+            {
+                props.top && <View style={{ flexDirection: 'row', width: '100%', marginTop: 7 }}>
+                    {props.icon}
+                    <Text style={{ textAlign: 'right', width: '90%', color: 'grey' }}> {Math.round(props.handler.value)}°C </Text>
+                </View>
+            }
+            <Slider
+                maximumValue={250}
+                minimumValue={0}
+                maximumTrackTintColor={colors.grey}
+                minimumTrackTintColor={colors.yellow}
+                step={5}
+                onValueChange={value => { props.handler.setValue(value); ReactNativeHapticFeedback.trigger("impactLight"); props.sendHandler(props.name, value) }}
+                value={props.handler.value}
+            />
+            {
+                props.bottom && <View style={{ flexDirection: 'row', width: '100%', marginTop: 7 }}>
+                    {props.icon}
+                    <Text style={{ textAlign: 'right', width: '90%', color: 'grey' }}> {Math.round(props.handler.value)}°C </Text>
+                </View>
+            }
+        </Fragment>
+    )
+}
+
+const CookContainer = (props) => {
+    const [topTemp, setTopTemp] = useState(180);
+    const [bottomTemp, setBottomTemp] = useState(80);
+    const [timeSlider, setTimeSlider] = useState(30);
+
+    return (
+    
+    <View style={[styles.autoContainer, {}]}>
+        <View style={{ flexDirection: 'row' }}>
+            <View style={[styles.detailsCircle, { backgroundColor: colors.yellow }]}>
+                <Icon name="utensils" size={14} color={colors.white} style={{ padding: 4, alignSelf: 'center' }} />
+            </View>
+            <Text style={styles.detailText}> Cook</Text>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+            <View style={{ width: '60%', marginLeft: 20 }}>
+                <TemperatureSlider top icon={<OvenTop height={28} width={28} fill={colors.black} />} handler={{ value: topTemp, setValue: setTopTemp }} />
+                <TemperatureSlider bottom icon={<OvenBottom height={28} width={28} fill={colors.black} />} handler={{ value: bottomTemp, setValue: setBottomTemp }} />
+            </View>
+            <View style={{ paddingLeft: 5, justifyContent: 'center' }}>
+                <CircularSlider
+                    step={2}
+                    min={0}
+                    max={90}
+                    value={timeSlider}
+                    onChange={setTimeSlider}
+                    contentContainerStyle={styles.contentContainerStyle}
+                    strokeWidth={4}
+                    buttonBorderColor={colors.blue}
+                    openingRadian={Math.PI / 4}
+                    buttonRadius={8}
+                    radius={40}
+                    linearGradient={[{ stop: '0%', color: colors.blue }, { stop: '100%', color: colors.blue }]}
+                >
+                    <Text style={styles.value}>{timeSlider}</Text>
+                    <Text style={styles.min}>min</Text>
+                </CircularSlider>
+            </View>
+        </View>
+    </View>)
+
+}
+
+export default function Timeline(props) {
+
+    return (
+        <View >
+            {
+                props.items[0].steps.map((item, i) => (
+                    <Fragment>
+                        <Text key={i}>{item.type}</Text>
+                        <CookContainer />
+                    </Fragment>
+                ))
+            }
+
+        </View>
+    );
+}
 
